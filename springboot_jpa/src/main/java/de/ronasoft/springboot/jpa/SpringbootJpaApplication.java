a428f97b7cc56f955b73a3c1c4dc51ba747d5b90
ronasoft <admin@ronasoft.de>
2019-03-31 11:55:45 +0200

    JPA Package umbenannt

--- springboot_jpa/src/main/java/de/ronasoft/springboot/jpa/SpringbootJpaApplication.java
+++ springboot_jpa/src/main/java/de/ronasoft/springboot/jpa/SpringbootJpaApplication.java
@@ -1,4 +1,4 @@
-package de.ronasoft.springboot.springboot_jpa;
+package de.ronasoft.springboot.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
