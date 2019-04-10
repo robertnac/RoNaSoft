473cdcf50553e357a368451d31c30ba397f7f0b5
ronasoft <admin@ronasoft.de>
2019-04-10 21:44:40 +0200

    JPA Repository Test hinzu

--- springboot_jpa/src/main/java/de/ronasoft/springboot/jpa/domain/single/Kunde.java
+++ springboot_jpa/src/main/java/de/ronasoft/springboot/jpa/domain/single/Kunde.java
@@ -21,6 +21,12 @@ public class Kunde {
 		this.vorname = vorname;
 		this.nachname = nachname;
 	}
+	
+	public Kunde(Long id, String vorname, String nachname) {
+		this.id = id;
+		this.vorname = vorname;
+		this.nachname = nachname;
+	}
 
 	@Override
 	public String toString() {
@@ -37,6 +43,37 @@ public class Kunde {
 
 	public String getNachname() {
 		return nachname;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nachname == null) ? 0 : nachname.hashCode());
+		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kunde other = (Kunde) obj;
+		if (nachname == null) {
+			if (other.nachname != null)
+				return false;
+		} else if (!nachname.equals(other.nachname))
+			return false;
+		if (vorname == null) {
+			if (other.vorname != null)
+				return false;
+		} else if (!vorname.equals(other.vorname))
+			return false;
+		return true;
 	}
 	
 	
---
+++ springboot_jpa/src/test/java/de/ronasoft/springboot/jpa/domain/single/KundeRepositoryTest.java
@@ -0,0 +1,64 @@
+package de.ronasoft.springboot.jpa.domain.single;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.ronasoft.springboot.jpa.SpringbootJpaApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KundeRepositoryTest {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(KundeRepositoryTest.class);
+
+	@Autowired
+	KundeRepository kundeRepository;
+	
+	@Test
+	public void testSaveAndFindById() {
+		Kunde kunde1 = new Kunde(1L, "Olaf", "Thon");
+		Kunde kunde2 = new Kunde(2L, "Guido", "Burgstaller");
+		
+		// speichern
+		kundeRepository.saveAll(Arrays.asList(kunde1, kunde2));
+		
+		// lesen
+		Optional<Kunde> optional = kundeRepository.findById(2L);
+		assertThat(optional).isNotEmpty(); // Kunde mit der Id 2 sollte gefunden werden.
+		
+		Kunde kunde = optional.get();
+		LOGGER.info("Gefunden [{}].", kunde);
+		assertThat(kunde.getVorname()).isEqualToIgnoringCase("Guido");
+		assertThat(kunde.getNachname()).isEqualToIgnoringCase("Burgstaller");
+	}
+	
+	@Test
+	public void testSaveAndFindByNachname() {
+		Kunde kunde1 = new Kunde(1L, "Olaf", "Thon");
+		Kunde kunde2 = new Kunde(2L, "Guido", "Burgstaller");
+		
+		// speichern
+		kundeRepository.saveAll(Arrays.asList(kunde1, kunde2));
+		
+		// lesen
+		List<Kunde> kunden = kundeRepository.findByNachname("Thon");
+		assertThat(kunden).contains(kunde1); // Kunde mit dem Nachnamen 'Thon' sollte gefunden werden
+		
+		Kunde kunde = kunden.get(0);
+		LOGGER.info("Gefunden [{}].", kunde);
+		assertThat(kunde.getVorname()).isEqualToIgnoringCase("Olaf");
+	}
+
+}
